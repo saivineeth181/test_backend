7cf4ad53d7af71d80996faa77efd0445349d3d28
saivineeth vuppala <saivineethvuppala@saivineeths-MacBook-Pro.local>
2025-08-20 20:47:56 +0530

    add new cors url

--- social_backend/settings.py
+++ social_backend/settings.py
@@ -142,6 +142,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://test-backend-tk1o.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
